6dd699b313051ccc5782056338122c26ec84f408
Farelle <farelleroderickputrae@webmail.umm.ac.id>
2025-11-23 19:36:09 +0700

    upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
-
-import 'app/routes/app_pages.dart';
+import 'app/modules/home/views/reset_password_screen.dart';
 
 void main() {
-  runApp(
-    GetMaterialApp(
-      title: "Application",
-      initialRoute: AppPages.INITIAL,
-      getPages: AppPages.routes,
-    ),
-  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: "Reset Password",
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/reset',
+      getPages: [
+        GetPage(
+          name: '/reset',
+          page: () => const ResetPasswordScreen(),
+        ),
+      ],
+    );
+  }
 }
